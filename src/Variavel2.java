497b3aa64064decf5d97f074a06b079bc745f8ac
Anderson Siqueira <contato@andersonls.com.br>
2021-02-12 21:19:41 -0300

    Conversão de valores

---
+++ src/Variavel2.java
@@ -0,0 +1,20 @@
+public class Variavel2 {
+    public static void main(String[] args){
+
+        // conversão de valores
+        int valor1 = 12;
+
+        long valorlong = valor1;
+        System.out.println(valorlong);
+
+        int valorInt = (int)valorlong;
+        System.out.println(valorInt);
+
+        double valorDouble = valorInt;
+        System.out.println(valorDouble);
+
+        long outroLong = (long)valorDouble;
+        System.out.println(outroLong);
+
+    }
+}
