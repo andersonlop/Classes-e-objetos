3912ab1d78c26f85959fe0f082ccf5937e319bf5
Anderson Siqueira <contato@andersonls.com.br>
2021-02-12 21:35:37 -0300

    Conversão de objetos de texto

---
+++ src/Variavel3.java
@@ -0,0 +1,19 @@
+public class Variavel3 {
+    public static void main(String[] args){
+        // Conversão de objetos de texto
+
+        char vetores[] = {'a', 'b', 'c'};
+        String texto = String.valueOf(vetores);
+        System.out.println(texto);
+
+        long longConvertido = 12;
+        String valor = String.valueOf(longConvertido);
+        System.out.println(valor);
+
+        Double ValorDouble = Double.valueOf(valor);
+        System.out.println(valor);
+
+
+
+    }
+}
