e36fb5feb5ece4c3dadefcaf852b59ecfd86b453
Anderson Siqueira <contato@andersonls.com.br>
2021-02-15 20:36:16 -0300

    Classes Math
    Classes utilitárias, para usar em cálculos matemáticos.
    
    * Método round;
    * Número randômico;
    * Buscando o maior número;

---
+++ src/Variavel7.java
@@ -0,0 +1,18 @@
+public class Variavel7 {
+    public static void main(String[] args){
+
+        // Classes Math - Classes utilitárias, para usar em cálculos matemáticos.
+
+        // Método round.
+        System.out.println(Math.round(15.4)); // Arrendondamento
+        System.out.println(Math.round(15.5));
+
+        // Número randômico
+        System.out.println(Math.random());
+
+        // Buscando o maior número
+        System.out.println(Math.max(1, 10));
+
+
+    }
+}
